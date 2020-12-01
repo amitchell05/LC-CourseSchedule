80baf24521ac9d9aa0d7532678b94aac03cf5d7a
Angela Mitchell <anglarenmit@gmail.com>
2020-11-30 19:21:11 -0600

    Course class defined.

--- course-schedule/src/app/course.model.ts
+++ course-schedule/src/app/course.model.ts
@@ -1,2 +1,9 @@
 export class Course {
+    constructor(
+        public title: string, 
+        public code: string, 
+        public days: string[], 
+        public startTime: string, 
+        public endTime: string, 
+        public credits: number) {};
 }
