f78fd4848df5c9fbc77d6f7efb81d09aa154141e
Angela Mitchell <anglarenmit@gmail.com>
2020-11-30 19:37:37 -0600

    Lists of courses added and basic structure for lists on page in place.

--- course-schedule/src/app/courses/courses.component.ts
+++ course-schedule/src/app/courses/courses.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Course } from '../course.model';
 
 @Component({
   selector: 'app-courses',
@@ -6,10 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./courses.component.css']
 })
 export class CoursesComponent implements OnInit {
+  allCourses: Course[] = [];
+  availableCourses: Course[] = [];
+  myCourses: Course[] = [];
 
   constructor() { }
 
   ngOnInit() {
+    this.allCourses = [
+      new Course("Intro to Underwater Coding", "LC105", ["Mon", "Wed"], "2:00 PM", "3:30 PM", 3),
+      new Course("Philosophy & Star Trek", "LC140", ["Tue", "Thu"], "9:00 AM", "10:30 AM", 3),
+      new Course("Googling Like a Boss", "LC115", ["Tue", "Thu"], "11:00 AM", "12:00 PM", 2),
+      new Course("Narcissism & Stack Overflow: A Case Study", "LC411", ["Mon", "Wed"], "2:30 PM", "4:00 PM", 3),
+      new Course("Coding Logic with Spock", "LC240", ["Tue", "Thu"], "8:30 AM", "10:00 AM", 3),
+      new Course("Irregular Expressions", "LC317", ["Tue", "Thu"], "2:00 PM", "3:30 PM", 3),
+      new Course("Demystifying Asynchronicity", "LC230", ["Wed", "Fri"], "2:00 PM", "3:00 PM", 2),
+      new Course("Intro to Angular Material", "LC205", ["Mon", "Wed"], "5:30 PM", "7:00 PM", 2),
+      new Course("The Mysteries of Git Solved", "LC410", ["Tue", "Thu"], "12:30 PM", "3:00 PM", 3),
+      new Course("HTML and CSS: Where Fun Lives", "LC102", ["Mon", "Wed"], "2:00 PM", "3:00 PM", 2)
+    ];
+
+    this.availableCourses = this.allCourses.slice(0);
+
   }
 
 }
